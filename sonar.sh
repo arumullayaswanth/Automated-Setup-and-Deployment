1501181449c4511f1825361a30fdfee1e804439c
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-03-02 19:15:41 +0530

    Update sonarqube.sh

--- sonar.sh
+++ sonar.sh
@@ -9,6 +9,7 @@ chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
 
+
 #run this on server manually
 #sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start
 #echo "user=admin & password=admin"
