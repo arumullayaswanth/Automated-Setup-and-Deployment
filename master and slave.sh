86bff24285700d0faa93cdbd60e0168956f569d3
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-03-03 13:12:44 +0530

    Update master and slave.sh

--- master and slave.sh
+++ master and slave.sh
@@ -2,7 +2,7 @@
 yum install git java-1.8.0-openjdk maven -y
 
 #STEP-2: DOWNLOAD JAVA11 AND JENKINS
-amazon-linux-extras install java-openjdk11 -y
-#sudo yum install java-17-amazon-corretto -y
+#amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-17-amazon-corretto -y
 #update-alternatives --config java
 #java -version
