478dc491a9001df6b069c47c388bb85e36ce4134
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-03-03 10:39:37 +0530

    Create master and slave.sh

---
+++ master and slave.sh
@@ -0,0 +1,8 @@
+#STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
+yum install git java-1.8.0-openjdk maven -y
+
+#STEP-2: DOWNLOAD JAVA11 AND JENKINS
+amazon-linux-extras install java-openjdk11 -y
+#sudo yum install java-17-amazon-corretto -y
+#update-alternatives --config java
+#java -version
